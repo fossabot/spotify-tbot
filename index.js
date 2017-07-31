09f5874ced045a14bd3714ae0923ddd5d6b3c5f9
W1m3R <ruto1924@gmail.com>
2017-07-31 20:19:27 +0900

    Modify webserver calling code

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-require('./dist')();
+'use strict';
+
+var _dist = require('./dist');
+var _dist2 = _interopRequireDefault(_dist);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+(0, _dist2.default)();
